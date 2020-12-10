4d261a05aa50bd959a759868f5f5f1c5530c5a4f
vaux <zero.griffin@gmail.com>
2020-12-10 15:45:51 +0100

    Configure project

---
+++ macOS/App.swift
@@ -0,0 +1,14 @@
+import AppKit
+
+@NSApplicationMain final class App: NSApplication, NSApplicationDelegate  {
+    required init?(coder: NSCoder) { nil }
+    override init() {
+        super.init()
+        delegate = self
+    }
+    
+    func applicationWillFinishLaunching(_: Notification) {
+//        mainMenu = Menu()
+        NSWindow().makeKeyAndOrderFront(nil)
+    }
+}
